1426e2ff07785647e0d80cfc9a6102d9016c7f4f
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-02-08 19:44:36 +0500

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,26 +5,25 @@ from app.routers.auth_router import router as auth_router
 
 
 app = FastAPI(
+    root_path=settings.api_prefix,
     title=settings.app_name,
     debug=settings.app_debug,
-    openapi_prefix=settings.api_prefix,
 )
 
 setup_cors_middleware(app)
 setup_session_middleware(app)
 
 
-api_router = APIRouter(prefix=settings.api_prefix)
+api_router = APIRouter()
+
+api_router.include_router(auth_router)
+app.include_router(api_router)
 
 
-@api_router.get("")
+@app.get("")
 async def read_root():
     return {
         "app_name": settings.app_name,
         "environment": settings.app_env,
         "debug_mode": settings.app_debug
     }
-
-api_router.include_router(auth_router)
-
-app.include_router(api_router)
