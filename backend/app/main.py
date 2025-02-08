c81c4fa3bb8efc064814225b4333d59dae5c0c31
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-02-08 18:10:42 +0500

    fix backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,24 @@
-import uvicorn
 from fastapi import FastAPI, APIRouter
 from app.core.config import settings
 from app.middleware.setup import setup_cors_middleware, setup_session_middleware
 from app.routers.auth_router import router as auth_router
-from starlette.middleware.cors import CORSMiddleware
 
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.app_debug,
+    openapi_prefix=settings.api_prefix,
+    docs_url="/docs",
+)
 
 setup_cors_middleware(app)
 setup_session_middleware(app)
 
 
+api_router = APIRouter(prefix="/api")
 
-api_router = APIRouter()
 
-@api_router.get("/")
+@api_router.get("")
 async def read_root():
     return {
         "app_name": settings.app_name,
@@ -23,4 +26,6 @@ async def read_root():
         "debug_mode": settings.app_debug
     }
 
-api_router.include_router(auth_router, prefix=settings.api_prefix, tags=["auth"])
+api_router.include_router(auth_router)
+
+app.include_router(api_router)
